c9d5e128067510521c1fd9fb1f1b255cff6f0799
Rishi004 <rishireiko@gmail.com>
2023-12-15 14:28:55 +0530

    initial commit

---
+++ app/dashboard/products/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductsPage() {
+    return <div>Products Page</div>;
+}
+
+export default ProductsPage;
---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UsersPage() {
+    return <div>Users Page</div>;
+}
+
+export default UsersPage;
